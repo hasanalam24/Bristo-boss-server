22d5ec608b818c108dbf599b5da507f206c044e1
hasanalam24 <hasanalam365@gmail.com>
2024-05-22 22:11:49 +0600

    uniqe user email check and loginIn

--- index.js
+++ index.js
@@ -35,6 +35,15 @@ async function run() {
         //user related api
         app.post('/users', async (req, res) => {
             const user = req.body;
+
+            //insert email if user doesn't exists
+            //you can do this many ways : (1. email unique, 2. upsert, 3.simple checking)
+
+            const query = { email: user.email }
+            const existUser = await usersCollections.findOne(query)
+            if (existUser) {
+                return res.send({ message: 'user already exists', insertedId: null })
+            }
             const result = await usersCollections.insertOne(user)
             res.send(result)
         })
