a3f8dc86384fc36d21b5bf073ffa88b010aaf4df
hasanalam24 <hasanalam365@gmail.com>
2024-05-26 22:13:04 +0600

    payment post method in the server

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express()
 const cors = require('cors');
 const jwt = require('jsonwebtoken')
 require('dotenv').config()
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY)
 const port = process.env.PORT || 5000
 
 
@@ -200,6 +201,27 @@ async function run() {
             const result = await cartCollections.deleteOne(query)
             res.send(result)
         })
+
+        //Payment Intent
+        app.post('/create-payment-intent', async (req, res) => {
+            const { price } = req.body;
+            const amount = parseInt(price * 100);
+
+            console.log('inside the server amount:', amount)
+
+            const paymentIntent = await stripe.paymentIntents.create({
+                amount: amount,
+                currency: 'usd',
+                payment_method_types: ['card']
+
+            })
+
+            res.send({
+                clientSecret: paymentIntent.client_secret
+            })
+
+        })
+
 
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
